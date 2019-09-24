73163a951452e585aae802d126dfe5d39c5058c2
Ludvig Bartholdsson <44546168+ludvigbartholdsson@users.noreply.github.com>
2019-09-24 17:03:42 +0200

    updated

--- server.lua
+++ server.lua
@@ -1,27 +1,33 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
 local auths = {}
 
 ESX.RegisterServerCallback('ludvig-pedmenu:addPedMenu', function(source, cb, args)
 
+    print(inArray(auths, args))
     if inArray(auths, args) then
-        table.remove(auths, args[1])
-        return false
+        table.remove(auths, tonumber(args))
+        cb(false)
     else
-        table.insert(auths, args[1])
-        return true
+        table.insert(auths, args)
+        cb(true)
     end
 end)
 
-ESX.RegisterServerCallback('ludvig-pedmenu:checkAccess', function(source, cb, args)
+ESX.RegisterServerCallback('ludvig-pedmenu:checkAccess', function(source, cb)
+    local src = ESX.GetPlayerFromId(source)
 
-    if inArray(auths, args) then
-        return false
+    if inArray(auths, src.identifier) then
+        cb(true)
     else
-        return true
+        cb(false)
     end
 end)
 
 --returns TRUE if value contains, else FALSE
-local inArray = function(tab, val)
+function inArray(tab, val)
     for index, value in ipairs(tab) do
         if value == val then
             return true
